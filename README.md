e8052584712479976bcff9c7def8051f5f4fa239
Matt Mendez Zugenbuehler <mzugenbuehler@gmail.com>
2025-01-17 18:37:31 -0800

    styling edits and css

---
+++ README.md
@@ -0,0 +1,30 @@
+# Vinyl Collection App
+My project is an app that allows you to track the records in your music collection, add your review (1 - 5 Stars), share your thoughts, and view other users collection.
+
+The app was designed as a simple way to know what albums you have and be able to take brief notes on which are your favorites, why, and then be able to view
+
+## Getting Started
+To run the app locally, you can follow the below steps:
+- Prerequesites: Node.js, MongoDB
+- Install:
+1. Clone the repository (https://github.com/mendezugenbuehler/vinyl-collection-app)
+2. Install dependencies (npm i)
+3. Create an .env file (incl. variables MONGODB_URI & SESSION_SECRET)
+4. Start the app (nodemon)
+5. Open your browser and go to http://localhost:3000 to use thet app
+
+### Wireframe
+
+## Technologies
+- Node.js
+- Express
+- MongoDB
+- Mongoose
+- EJS (Embedded JavaScript)
+- JavaScript
+
+## Next Steps & Future Enhancements
+- Additional user features to search, filter, and add a wish list
+- Incorporate album art work via API
+- Embed a map with links to nearby record shops submitted by users
+- Dark mode 
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ dotenv.config();
 
 const passUserToView = require('./middleware/pass-user-to-view.js');
 const isSignedIn = require('./middleware/is-signed-in.js');
+const User = require('./models/user.js'); // Import the User model
 
 const authController = require('./controllers/auth.js');
 const usersController = require('./controllers/users.js');
@@ -15,6 +16,7 @@ const recordsController = require('./controllers/records.js');
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
 app.use(methodOverride('_method'));
 app.use(session({
     secret: process.env.SESSION_SECRET,
@@ -23,32 +25,33 @@ app.use(session({
 }));
 app.use(passUserToView);
 
+// Routes
 app.use('/auth', authController);
-
 app.use(isSignedIn);
-
 app.use('/users', usersController);
-
 app.use('/users/:userId/records', recordsController);
 app.use('/records', recordsController);
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/record-collection', { useNewUrlParser: true, useUnifiedTopology: true });
+// MongoDB Connection
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/record-collection', { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+});
 mongoose.connection.on('connected', () => {
     console.log('Connected to MongoDB');
 });
 
-
+// Root Route
 app.get('/', (req, res) => {
     if (!req.session.user) {
-        return res.render('index.ejs', { user: null, records: [] });
+        // For non-logged-in users
+        return res.render('index.ejs', { user: null });
     }
-
-    const user = req.session.user;
-    const records = user.recordCollection || [];
-    res.render('index.ejs', { user, records });
+    // For logged-in users, show a personalized welcome
+    res.render('index.ejs', { user: req.session.user });
 });
 
-
+// Start the Server
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running...');
 });
